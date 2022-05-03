42d941351b742c9e44db5e49dc43e8564a6b405b
saad lahlou <saadlahlou.m@gmail.com>
2022-05-03 17:54:39 +0000

    pagination

--- src/main/java/emsi/ma/patientmvc2/PatientMvc2Application.java
+++ src/main/java/emsi/ma/patientmvc2/PatientMvc2Application.java
@@ -6,7 +6,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 
+import java.awt.print.Pageable;
 import java.sql.SQLOutput;
 import java.util.Date;
 import java.util.List;
@@ -22,17 +25,19 @@ public class PatientMvc2Application implements CommandLineRunner {
 
     @Override
     public void run(String... args) throws Exception {
+        for(int i=0; i<100;i++){
         patientRepository.save(
-                new Patient(null,"Hassane", new Date(),false,300));
-        patientRepository.save(
-                new Patient(null,"Mohamed", new Date(),false,300));
-        patientRepository.save(
-                new Patient(null,"Imane", new Date(),false,300));
-        patientRepository.save(
-                new Patient(null,"saad", new Date(),false,300));
-        List<Patient> patients = patientRepository.findAll();
+                new Patient(null,"Hassane", new Date(),Math.random()>0.5?true:false,(int)(Math.random()*100)));
+        }
 
-        patients.forEach(p-> {
+
+        Page<Patient> patients = patientRepository.findAll(PageRequest.of(1,10));
+        System.out.println("total pages :"+patients.getTotalPages());
+        System.out.println("total elements :"+patients.getTotalElements());
+        System.out.println("Num Page :"+patients.getNumber());
+        List<Patient> content = patients.getContent();
+        Page<Patient> byMalade = patientRepository.findByMalade(true, PageRequest.of(0,4));
+        byMalade.forEach(p-> {
             System.out.println("========");
             System.out.println(p.getId());
             System.out.println(p.getNom());
--- src/main/java/emsi/ma/patientmvc2/repositories/PatientRepository.java
+++ src/main/java/emsi/ma/patientmvc2/repositories/PatientRepository.java
@@ -1,7 +1,17 @@
 package emsi.ma.patientmvc2.repositories;
 
 import emsi.ma.patientmvc2.entities.Patient;
+import org.apache.catalina.LifecycleState;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface PatientRepository extends JpaRepository<Patient,Long> {
+    public List<Patient> findByMalade(boolean m);
+    Page<Patient> findByMalade(boolean m, Pageable pageable);
+
+
+
 }
